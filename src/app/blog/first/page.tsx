0c98a9f64ec840a60b92bfb0697f4a2297c44e66
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-11-22 12:08:50 +0530

    Routes & Nested routes 🛣️

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,7 @@
+const FirstBlog = () => {
+  return (
+    <div className="flex justify-center items-center">FirstBlog</div>
+  )
+}
+
+export default FirstBlog
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+const SecondPage = () => {
+  return (
+    <div className="flex justify-center items-center">SecondPage</div>
+  )
+}
+
+export default SecondPage
