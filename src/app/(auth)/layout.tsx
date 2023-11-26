9a49c258acc8fb19311384bff7dff43199299543
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-11-26 13:36:47 +0530

    Navigating Programmatically 🔗

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const navLinks = [
     { href: "/login", label: "Login" },
@@ -14,13 +14,21 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     const pathname = usePathname();
+    const router = useRouter();
+
+    const handleNavigate = () => {
+        router.push("/");
+        // router.replace("/");
+        // router.back();      // Backward routes
+        // router.forward();   // Forward routes
+    }    
 
     return (
         <div className="w-full min-h-screen">
             <div className="mt-12 flex flex-row gap-8 justify-center items-center text-blue-300">
                 {navLinks.map((link) => {
                     const isActive = pathname.startsWith(link.href);
-                    
+
                     return(
                         <Link 
                             href={link.href} 
@@ -33,8 +41,14 @@ export default function AuthLayout({
                     )
                 })}
             </div>
-            <div className="flex justify-center mt-16">
+            <div className="flex flex-col justify-center items-center mt-16">
                 {children}
+                <button
+                    className="mt-8 py-3 px-8 bg-gray-900 hover:bg-gray-800 font-semibold"
+                    onClick={handleNavigate}
+                >
+                    Back to Home
+                </button>
             </div>
         </div>
     )
