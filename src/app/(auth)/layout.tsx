e05cad9413b87d9a0a40c196703346d37666a3b4
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-11-26 12:57:26 +0530

    Active links 🔗💡

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,38 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+    { href: "/login", label: "Login" },
+    { href: "/register", label: "Register" },
+];
+
 export default function AuthLayout({
     children,
 } : {
     children: React.ReactNode;
 }) {
+    const pathname = usePathname();
+
     return (
         <div className="w-full min-h-screen">
-            <h2>Auth Page</h2>
+            <div className="mt-12 flex flex-row gap-8 justify-center items-center text-blue-300">
+                {navLinks.map((link) => {
+                    const isActive = pathname.startsWith(link.href);
+                    
+                    return(
+                        <Link 
+                            href={link.href} 
+                            key={link.href}
+                            // className={ pathname === link.href ? "text-orange-300" : "" }
+                            className={ isActive ? "text-orange-300" : "" }
+                        >
+                            {link.label}
+                        </Link>
+                    )
+                })}
+            </div>
             <div className="flex justify-center mt-16">
                 {children}
             </div>
